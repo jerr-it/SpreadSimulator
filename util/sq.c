dee0af182774ba17195af3f42215ebe06c228b29
Jerrit Gläsker <50867655+cherrysrc@users.noreply.github.com>
2020-10-20 16:59:12 +0200

    Adjusted Fast Inverse Square Root

--- util/sq.c
+++ util/sq.c
@@ -1,22 +1,19 @@
 /**
  * Performs Fast inverse square root
- * https://en.wikipedia.org/wiki/Fast_inverse_square_root
- * @param number x
+ * https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Approximations_that_depend_on_the_floating_point_representation
+ * @param x x
  * @returns 1/sqrt(x)
  */
-static float InverseSquare(float number)
+static float InverseSquare(float x)
 {
-    long i;
-    float x2, y;
-    const float threehalfs = 1.5;
-
-    x2 = number * 0.5;
-    y = number;
-
-    i = *(long *)&y;
-    i = 0x5f3759df - (i >> 1);
-    y = *(float *)&i;
-    y = y * (threehalfs - (x2 * y * y));
-
-    return y;
+    float xhalf = 0.5f * x;
+    union {
+        float x;
+        int i;
+    } u;
+    u.x = x;
+    u.i = 0x5f375a86 - (u.i >> 1);
+    /* The next line can be repeated any number of times to increase accuracy */
+    u.x = u.x * (1.5f - xhalf * u.x * u.x);
+    return u.x;
 }
